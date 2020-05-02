700bbc85a623eef05d26ab34899098c420100575
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-02 12:12:17 +0300

    Move mock data to txt files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,22 +1,24 @@
 "use strict";
 
-const {writeFile} = require(`fs`).promises;
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
 const {getRandomInt, shuffle} = require(`../../utils`);
-const {TITLES, SENTENCES, CATEGORIES} = require(`../../data`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const generateOffers = (count) => {
-  const title = TITLES[getRandomInt(0, TITLES.length - 1)];
+const generateOffers = (count, titles, sentences, categories) => {
+  const title = titles[getRandomInt(0, titles.length - 1)];
   const createdDate = new Date();
-  const announce = shuffle(SENTENCES).slice(0, getRandomInt(1, 4));
-  const fullText = shuffle(SENTENCES)
-    .slice(0, getRandomInt(0, SENTENCES.length - 1))
+  const announce = shuffle(sentences).slice(0, getRandomInt(1, 4));
+  const fullText = shuffle(sentences)
+    .slice(0, getRandomInt(0, sentences.length - 1))
     .join(` `);
-  const category = shuffle(CATEGORIES).slice(0, getRandomInt(1, 4));
+  const category = shuffle(categories).slice(0, getRandomInt(1, 4));
 
   return Array(count)
     .fill({})
@@ -29,15 +31,37 @@ const generateOffers = (count) => {
     }));
 };
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf-8`);
+
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+
+    return [];
+  }
+};
+
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const [titles, sentences, categories] = await Promise.all([
+      await readContent(FILE_TITLES_PATH),
+      await readContent(FILE_SENTENCES_PATH),
+      await readContent(FILE_CATEGORIES_PATH),
+    ]);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer), null, 2);
+    const content = JSON.stringify(
+        generateOffers(countOffer, titles, sentences, categories),
+        null,
+        2
+    );
 
     try {
-      await writeFile(FILE_NAME, content);
+      await fs.writeFile(FILE_NAME, content);
 
       console.log(chalk.green(`Operation success. File created`));
     } catch (err) {
