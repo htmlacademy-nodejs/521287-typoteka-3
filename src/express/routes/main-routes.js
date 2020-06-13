9e6527e5a9f5272d360c2e92e577b205fb19608a
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 00:29:09 +0300

    Template index page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
 const {Router} = require(`express`);
 
+const ROOT = `main`;
+
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
+mainRouter.get(`/`, (req, res) => res.render(`${ROOT}/main`));
 mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/login`, (req, res) => res.send(`/login`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
