19945099f62242cfb6a4be16892f69a0bd61a93d
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 13:41:01 +0300

    Template sign-up page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,7 +7,7 @@ const ROOT = `main`;
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => res.render(`${ROOT}/main`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
+mainRouter.get(`/sign-up`, (req, res) => res.render(`${ROOT}/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.send(`/login`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
 mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
