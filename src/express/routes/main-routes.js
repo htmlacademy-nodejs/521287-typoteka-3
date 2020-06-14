1493cb49ed225a3776539246333f866413cad3ae
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 14:49:45 +0300

    Template search page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -9,7 +9,7 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.render(`${ROOT}/main`));
 mainRouter.get(`/sign-up`, (req, res) => res.render(`${ROOT}/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`${ROOT}/login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/search`, (req, res) => res.render(`${ROOT}/search`));
 mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
 
 module.exports = mainRouter;
