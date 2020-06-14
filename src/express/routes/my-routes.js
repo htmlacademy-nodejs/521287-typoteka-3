6ee22828de5ec0cec9ea3bc2db1f3b3afd4cef6e
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 15:08:25 +0300

    Template my page

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,10 +2,11 @@
 
 const {Router} = require(`express`);
 
-const ROOT = `/my`;
+const ROOT = `my`;
+
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(ROOT));
-myRouter.get(`/comments`, (req, res) => res.send(`${ROOT}/comments`));
+myRouter.get(`/`, (req, res) => res.render(`${ROOT}/my`));
+myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
 
 module.exports = myRouter;
