ad0762ea8bc505788cb294880b4fab4192a753ed
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 15:57:25 +0300

    Template comments page

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -7,6 +7,6 @@ const ROOT = `my`;
 const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => res.render(`${ROOT}/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/comments`, (req, res) => res.render(`${ROOT}/comments`));
 
 module.exports = myRouter;
