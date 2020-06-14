dcc7559544ec72b86de824b5cb362d5ab1e54bf4
Ilya Sidorchik <ilya@sidorchik.ru>
2020-06-14 14:57:44 +0300

    Template categories page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -10,6 +10,6 @@ mainRouter.get(`/`, (req, res) => res.render(`${ROOT}/main`));
 mainRouter.get(`/sign-up`, (req, res) => res.render(`${ROOT}/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`${ROOT}/login`));
 mainRouter.get(`/search`, (req, res) => res.render(`${ROOT}/search`));
-mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/categories`, (req, res) => res.render(`${ROOT}/categories`));
 
 module.exports = mainRouter;
