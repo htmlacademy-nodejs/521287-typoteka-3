0db8c39053ca666355c205513153e3a8ac66cdd6
Ilya Sidorchik <ilya@sidorchik.ru>
2021-01-10 18:58:55 +0300

    4.3 Handle data for index page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,11 +2,18 @@
 
 const {Router} = require(`express`);
 
+const api = require(`../api`).getAPI();
+
 const ROOT = `main`;
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`${ROOT}/main`));
+mainRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+
+  res.render(`${ROOT}/main`, {articles});
+});
+
 mainRouter.get(`/sign-up`, (req, res) => res.render(`${ROOT}/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`${ROOT}/login`));
 mainRouter.get(`/search`, (req, res) => res.render(`${ROOT}/search`));
