d51defc28a02d568e58a9622d433d7f90b10ec07
Ilya Sidorchik <ilya@sidorchik.ru>
2021-01-11 19:47:50 +0300

    4.3 Do not store article pictures

--- src/express/index.js
+++ src/express/index.js
@@ -7,11 +7,14 @@ const router = require(`./routes`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
+const UPLOAD_DIR = `upload`;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(router);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
