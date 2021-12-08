4b7ce77643bdcab0a79a58c14681f59010d11d5a
Ilya Sidorchik <ilya@sidorchik.ru>
2021-12-08 23:51:33 +0300

    7.2 Add users data service

--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -4,10 +4,12 @@ const ArticleService = require(`./articles`);
 const CategoryService = require(`./categories`);
 const SearchService = require(`./search`);
 const CommentService = require(`./comments`);
+const UserService = require(`./users`);
 
 module.exports = {
   ArticleService,
   CategoryService,
   SearchService,
   CommentService,
+  UserService,
 };
--- src/service/data-service/search.js
+++ src/service/data-service/search.js
@@ -7,6 +7,7 @@ const Aliase = require(`~/service/models/aliase`);
 class SearchService {
   constructor(sequelize) {
     this._Article = sequelize.models.Article;
+    this._User = sequelize.models.User;
   }
 
   async findAll(searchText) {
@@ -16,7 +17,19 @@ class SearchService {
           [Op.substring]: searchText,
         }
       },
-      include: [Aliase.CATEGORIES],
+      include: [
+        Aliase.CATEGORIES,
+        {
+          model: this._User,
+          as: Aliase.USERS,
+          attributes: {
+            exclude: [`passwordHash`]
+          }
+        }
+      ],
+      order: [
+        [`createdAt`, `DESC`]
+      ]
     });
     const result = articles.map((article) => article.get());
 
