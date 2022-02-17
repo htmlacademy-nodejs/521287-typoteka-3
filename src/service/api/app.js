77850d1a0a0391b96232ad9ea3c5a3df109fd47a
Ilya Sidorchik <ilya@sidorchik.ru>
2022-02-17 16:34:01 +0300

    Fix B10 criteria: remove logic from index files

---
+++ src/service/api/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const {Router} = require(`express`);
+
+const sequelize = require(`~/service/lib/sequelize`);
+const defineModels = require(`~/service/models`);
+const {
+  ArticleService,
+  CategoryService,
+  SearchService,
+  CommentService,
+  UserService,
+} = require(`~/service/data-service`);
+
+const articles = require(`./articles/articles`);
+const comments = require(`./comments/comments`);
+const categories = require(`./categories/categories`);
+const search = require(`./search/search`);
+const users = require(`./users/users`);
+
+const app = new Router();
+
+defineModels(sequelize);
+
+(async () => {
+  articles(app, new ArticleService(sequelize), new CommentService(sequelize));
+  comments(app, new CommentService(sequelize));
+  categories(app, new CategoryService(sequelize));
+  search(app, new SearchService(sequelize));
+  users(app, new UserService(sequelize));
+})();
+
+module.exports = app;
